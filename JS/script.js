19ec8af9c6e469ddda67394bbf70d54aca55d206
Xerlok <20791727+Xerlok@users.noreply.github.com>
2023-03-04 21:44:27 +0300

    Complete game logic
    RockPS UI v1.0

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+const pScore = document.getElementById('player-result-box');
+const cScore = document.getElementById('pc-result-box');
+
 function getComputerChoice() {
     let computerChoice;
     let randomNumber;
@@ -21,13 +24,12 @@ function getComputerChoice() {
 }
 
 function playRound(computerSelection, playerSelection)  {
-    let result;
+    let playerScore = 0;
+    let pcScore = 0;
     playerSelection = playerSelection.toUpperCase();
     const pcImg = document.querySelector('#pc-img');
     const playerImg = document.querySelector('#player-img');
     const resultText = document.querySelector('#result');
-
-    console.log(playerSelection, computerSelection);
 
     if (playerSelection === 'ROCK') {
         playerImg.src = '../img/rock.png';
@@ -36,19 +38,18 @@ function playRound(computerSelection, playerSelection)  {
                 pcImg.src = '../img/rock.png';
                 resultText.textContent = `Tie.`;
                 resultText.style.color = 'black';
-                result = 1;
                 break;
             case 'Paper':
                 pcImg.src = '../img/paper.png';
                 resultText.textContent = `You lose! :^)`;
                 resultText.style.color = 'red';
-                result = 0;
+                pcScore += 1;
                 break;
             case 'Scissors':
                 pcImg.src = '../img/scissors.png';
                 resultText.textContent = `You win!`;
                 resultText.style.color = 'green';
-                result = 2;
+                playerScore += 1;
                 break;
         }
     }
@@ -60,19 +61,18 @@ function playRound(computerSelection, playerSelection)  {
                 pcImg.src = '../img/rock.png';
                 resultText.textContent = `You win!`;
                 resultText.style.color = 'green';
-                result = 2;
+                playerScore += 1;
                 break;
             case 'Paper':
                 pcImg.src = '../img/paper.png';
                 resultText.textContent = `Tie.`;
                 resultText.style.color = 'black';
-                result = 1;
                 break;
             case 'Scissors':
                 pcImg.src = '../img/scissors.png';
                 resultText.textContent = `You lose! :^)`;
                 resultText.style.color = 'red';
-                result = 0;
+                pcScore += 1;
                 break;
         }
     }
@@ -84,47 +84,44 @@ function playRound(computerSelection, playerSelection)  {
                 pcImg.src = '../img/rock.png';
                 resultText.textContent = `You lose! :^)`;
                 resultText.style.color = 'red';
-                result = 0;
+                pcScore += 1;
                 break;
             case 'Paper':
                 pcImg.src = '../img/paper.png';
                 resultText.textContent = `You win!`;
                 resultText.style.color = 'green';
-                result = 2;
+                playerScore += 1;
                 break;
             case 'Scissors':
                 pcImg.src = '../img/scissors.png';
                 resultText.textContent = `Tie.`;
                 resultText.style.color = 'black';
-                result = 1;
                 break;
         }
     }
 
-    else {
-        resultText.textContent ='Please write correct word.';
+    pScore.textContent = parseInt(pScore.textContent) + playerScore;
+    cScore.textContent = parseInt(cScore.textContent) + pcScore;
+
+    if (pScore.textContent == 5) {
+        resultText.textContent = 'You win filthy human!';
+        resultText.style.cssText = ('color:green; font-size: 5rem');
+    }
+
+    else if (cScore.textContent == 5) {
+        resultText.textContent = 'I win stupid human!';
+        resultText.style.cssText = ('color:red; font-size: 5rem');
     }
 
 
-    return result;
 }
 
 function game () {
     const btns = document.querySelectorAll('button');
-    btns.forEach((btn) => {btn.addEventListener('click', () => {playRound(getComputerChoice(), btn.id);});
+    btns.forEach((btn) => {btn.addEventListener('click', () => {
+        playRound(getComputerChoice(), btn.id); 
+    });
     }); 
-
-    /* if (playerScore > computerScore) {
-        resultText.textcontent =(`You win! Your score: ${playerScore}. My Score: ${computerScore}.`);
-    }
-
-    else if (playerScore < computerScore) {
-        resultText.textcontent =(`You lose! Your score: ${playerScore}. My Score: ${computerScore}.`);
-    }
-
-    else if (playerScore === computerScore) {
-        resultText.textcontent =(`It's a tie. Your score: ${playerScore}. My Score: ${computerScore}.`);
-    } */
  }  
 
 game();
